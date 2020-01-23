c560d236fbef9c8fa90e8cfc100db230e6f67981
Albert Ahmetzyanov <albert4reg@mail.ru>
2020-01-23 16:33:00 +0300

    String Trim

---
+++ ExtensionLibrary/StringExt/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionLibrary.StringExt
+{
+    public static class StringExtension
+    {
+        public static string LtcTrim(this string value)
+        {
+            if (value == null)
+                return string.Empty;
+
+            return value.Trim();
+        }
+    }
+}
---
+++ ExtensionLibrary_Test/StringExt_Test/TrimTest.cs
@@ -0,0 +1,24 @@
+﻿using ExtensionLibrary.StringExt;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ExtensionLibrary_Test.StringExt_Test
+{
+    public class TrimTest
+    {
+        [Theory]
+        [InlineData("Norm", "Norm")]
+        [InlineData("  Before", "Before")]
+        [InlineData("After ", "After")]
+        [InlineData("  Кругом ", "Кругом")]
+        [InlineData(null, "")]
+        public void TestTrim(string value, string result)
+        {
+            string extRes = value.LtcTrim();
+
+            Assert.Equal(result, extRes);
+        }
+    }
+}
