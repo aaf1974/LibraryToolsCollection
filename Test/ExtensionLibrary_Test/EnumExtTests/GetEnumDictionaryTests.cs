d4c3062b598a88e9d5460fee5a508d8d61183cc4
Albert Ahmetzyanov <albert4reg@mail.ru>
2020-02-06 17:16:35 +0300

    Enum extension implements

---
+++ Test/ExtensionLibrary_Test/EnumExtTests/GetEnumDictionaryTests.cs
@@ -0,0 +1,26 @@
+﻿using ExtensionLibrary.EnumExt;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtensionLibraryTest.EnumExtTests
+{
+    public class GetEnumDictionaryTests
+    {
+        Dictionary<SampleEnum, string> expectedDictionary = new Dictionary<SampleEnum, string>()
+        {
+            { SampleEnum.One, "OneDecription"},
+            { SampleEnum.Two, string.Empty},
+            { SampleEnum.Three, "ThreeDisplay"},
+        };
+
+
+        [Fact]
+        public void GetDictionaryTest()
+        {
+            Dictionary<SampleEnum, string> dic = SampleEnum.One.LtcGetEnumDictionary();
+
+            dic.Should().BeEquivalentTo(expectedDictionary);
+        }
+    }
+}
