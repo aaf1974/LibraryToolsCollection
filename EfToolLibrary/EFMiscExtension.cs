c088128c184c8c9522e5427af832e23b7dbdf1ef
Albert Ahmetzyanov <albert4reg@mail.ru>
2020-06-15 16:34:53 +0300

    CascadeDeleteOff

---
+++ EfToolLibrary/EFMiscExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace EfToolLibrary
+{
+    public static class EFMiscExtension
+    {
+        public static void LtcCascadeDeleteOff(this ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+    }
+}
